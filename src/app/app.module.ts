a974cbce50967cc34b2ae4204da7227cd9b5651a
Yonathan Seleshi <yonathan.seleshi@gmail.com>
2018-08-22 12:23:49 -0700

    Clean up dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { FavoritesPage} from "../pages/favorites/favorites";
+import {SettingsPage} from "../pages/settings/settings";
+import { LibraryPage} from "../pages/library/library";
+import {QuotePage} from "../pages/quote/quote";
+import {QuotesPage} from "../pages/quotes/quotes";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LibraryPage,
+    SettingsPage,
+    QuotePage,
+    QuotesPage,
+    FavoritesPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    SettingsPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage
+
   ],
   providers: [
     StatusBar,
